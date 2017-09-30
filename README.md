3e24bcd59af67bef57310888c58074cf00b9e4f9
Ben Turner <bbgrabbag@gmail.com>
2017-09-30 14:37:11 -0600

    base project initialized. Ready to start tutorial

---
+++ README.md
@@ -0,0 +1,12 @@
+##########################
+#REACT_USER_AUTH_TUTORIAL#
+##########################
+
+*INCLUDES:*
+- /react-auth-start: express server with auth and todo routes. Assumes prior knowledge of backend user authentication. Contains a wireframe front-end interface with react-router and redux from which to start implementing a user authentication feature.
+
+- /react-auth-complete: finished project
+
+**GOAL**
+- Learn how to implement a basic user authentication feature within a React application.
+- Follow tutorial <LINK HERE>
---
+++ react-auth-start/client/src/redux/actions/index.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+const todoUrl = "http://localhost:5000/todo/";
+
+function setTodos(todos) {
+    return {
+        type: "SET_TODOS",
+        todos
+    }
+}
+
+export function loadTodos() {
+    return (dispatch) => {
+        axios.get(todoUrl)
+            .then((response) => {
+                dispatch(setTodos(response.data));
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    }
+}
+
+export function addTodo(todo) {
+    return (dispatch) => {
+        axios.post(todoUrl, todo)
+            .then((response) => {
+                dispatch(loadTodos());
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    }
+}
+
+export function editTodo(id, todo) {
+    return (dispatch) => {
+        axios.put(todoUrl + id, todo)
+            .then((response) => {
+                dispatch(loadTodos());
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    }
+}
+
+export function deleteTodo(id){
+    return (dispatch) => {
+        axios.delete(todoUrl + id)
+            .then((response) => {
+                dispatch(loadTodos());
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    } 
+}
