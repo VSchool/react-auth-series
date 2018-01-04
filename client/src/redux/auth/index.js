d0a12e05e514237a917dc7fa5f2e1521dbdf09e8
Bob Ziroll <elderziroll@gmail.com>
2018-01-04 15:23:01 -0700

    Signup now saves user and token in store and localstorage

--- client/src/redux/auth/index.js
+++ client/src/redux/auth/index.js
@@ -7,9 +7,38 @@ export function signup(userInfo) {
                 const {token, user} = response.data;
                 localStorage.token = JSON.stringify(token);
                 localStorage.user = JSON.stringify(user);
+                dispatch(login(user, token));
             })
             .catch(err => {
                 console.error(err);
             })
+    }
+}
+
+export function login(user, token) {
+    return {
+        type: "LOGIN",
+        user: {
+            ...user,
+            token
+        }
+    }
+}
+
+const initialState = {
+    user: {},
+    isAuthenticated: false
+}
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case "LOGIN":
+            return {
+                ...state,
+                ...action.user,
+                isAuthenticated: true
+            }
+        default:
+            return state;
     }
 }
