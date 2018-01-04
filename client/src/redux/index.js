07438ffe27463dc3247417a9948614985c83b844
Bob Ziroll <elderziroll@gmail.com>
2018-01-04 15:23:01 -0700

    Signup now saves user and token in store and localstorage

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -2,9 +2,15 @@ import {combineReducers, createStore, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 
 import todos from "./todos";
+import user from "./auth";
 
 const reducer = combineReducers({
-    todos
+    todos,
+    user
 });
 
-export default createStore(reducer, applyMiddleware(thunk));
+export default createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+);
