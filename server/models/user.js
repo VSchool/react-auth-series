65dac2e53d3e11ab2f379464510f538a3d74a078
Bob Ziroll <elderziroll@gmail.com>
2018-01-05 14:38:05 -0700

    Remove name prop from user

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: String,
     username: {
         type: String,
         required: true,
