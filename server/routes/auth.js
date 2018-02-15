9f6c57347ceda508c3c1e31f8899f22e270edfbb
Bob Ziroll <elderziroll@gmail.com>
2018-02-15 14:34:38 -0700

    Semicolon

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require("express");
 const User = require("../models/user");
 const authRouter = express.Router();
 const jwt = require("jsonwebtoken");
