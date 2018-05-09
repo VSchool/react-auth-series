6bed19957dab6922e9be4c91cef162a43b4ef7f8
Ben Turner <bbgrabbag@gmail.com>
2018-05-09 09:36:02 -0600

    fixed todo id reference in TodoContainer

--- client/src/main/Todos/TodoContainer.js
+++ client/src/main/Todos/TodoContainer.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import Todo from "./Todo";
-import {connect} from "react-redux";
-import {editTodo, deleteTodo} from "../../redux/todos";
+import { connect } from "react-redux";
+import { editTodo, deleteTodo } from "../../redux/todos";
 
 class TodoContainer extends Component {
-    
-    handleCompleted(e){
-        this.props.editTodo(this.props.todo._id, {completed: e.target.checked})
+
+    handleCompleted(e) {
+        this.props.editTodo(this.props.todo._id, { completed: e.target.checked })
     }
 
-    handleRemove(){
+    handleRemove() {
         this.props.deleteTodo(this.props.todo._id);
     }
 
     render() {
         return (
             <Todo
-            handleCompleted={this.handleCompleted.bind(this)}
-            handleRemove={this.handleRemove.bind(this)}
+                handleCompleted={this.handleCompleted.bind(this)}
+                handleRemove={this.handleRemove.bind(this)}
                 todo={this.props.todo}
-                />
+            />
         )
     }
 }
 
-export default connect(null,{editTodo, deleteTodo})(TodoContainer);
+export default connect(null, { editTodo, deleteTodo })(TodoContainer);
